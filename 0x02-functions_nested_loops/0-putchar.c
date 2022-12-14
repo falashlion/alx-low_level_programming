9e0c30e0eb377467f48f82541ac0d09a5dec0570
fochu felix awah <forchufelix@gmail.com>
2022-12-13 18:26:13 -0800

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - prints _putchar, followed by a new line 
+* Return: always 0 (success)
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
