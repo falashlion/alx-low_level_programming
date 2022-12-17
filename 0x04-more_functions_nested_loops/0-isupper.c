f4deacf236e275719d8aeea0f4f4c8c960814bad
fochu felix awah <forchufelix@gmail.com>
2022-12-16 18:54:06 -0800

    nested

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
+
+
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
-*/
+
+ * _isupper - checks for uppercase character
+
+ * @c: the character to be checked
+
+ * Return: 1 if c is uppercase, 0 otherwise
+
+ */
+
 int _isupper(int c)
+
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+
+	return (c >= 'A' && c <= 'Z');
+
 }
